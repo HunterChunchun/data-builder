9554b642873c301b492196058247b31afb2fe132
hunterchun <951116853@qq.com>
2020-10-23 00:24:56 +0800

    first commit

---
+++ kafka-builder/src/main/java/org/hunter/stream/kafka/builder/core/FirstClass.java
@@ -0,0 +1,12 @@
+package org.hunter.stream.kafka.builder.core;
+
+/**
+ * @author hunter
+ * @des
+ * @date 2020/10/23
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
